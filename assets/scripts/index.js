2dc3bb738a862a13d474ee3e5467b1e603ea04e6
ND56 <ndrew56@gmail.com>
2018-04-08 12:04:50 -0400

    Implemented Web Socket code.
    (1) Fixed the config file so I wouldn't have to refer to the API URL in
    a convoluted way.
    (2) installed socket.io node package
    (3) Client emits to the dev and production API URLs
    (4) Added socket error handlers and test socket event code to
    demonstrate that the socket relationship is functional.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,34 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+// ran npm install socket.io
+const io = require('socket.io-client')
+const apiUrl = require('./config')
 
 $(() => {
-  // your JS code goes here
+  // something
+  // something
+
+  /**
+   * Adding web socket code.
+   */
+
+  // emitting to dev and production API
+  const socket = io(apiUrl)
+
+  // connection error handler
+  socket.on('connect_error', function (error) {
+    console.log('socket connection error is', error)
+  })
+  // standard error handler
+  socket.on('error', function (error) {
+    console.log('socket error is', error)
+  })
+  // testing a custom emission
+  socket.on('connect', function () {
+    socket.emit('greeting', 'Web sockets test. This message was sent from a client to the API and then broadcasted out to all clients.')
+  })
+  // testing receiving custom broadcast
+  socket.on('greeting', function (message) {
+    console.log(message)
+  })
 })
